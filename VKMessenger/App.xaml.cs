804898f343214fe2cf23d4c38af515d7e57105a8
Denis Kubashevskiy <dehax12@gmail.com>
2016-08-19 00:13:59 +0300

    FIXED #6. Исправлена ошибка авторизации в новом дизайне ВКонтакте.

--- VKMessenger/App.xaml.cs
+++ VKMessenger/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -17,95 +18,139 @@ using VKMessenger.ViewModel;
 
 namespace VKMessenger
 {
-    public partial class App : Application
-    {
-        private const string LOG_FILE_NAME = @"VKMessenger.log";
+	public partial class App : Application
+	{
+		private const string LOG_FILE_NAME = @"VKMessenger.log";
 
-        private Messenger _messenger = new Messenger();
+		private Messenger _messenger = new Messenger();
 
-        public App()
-        {
-            ShutdownMode = ShutdownMode.OnExplicitShutdown;
-            DispatcherUnhandledException += ProcessUnhandledException;
-        }
+		public App()
+		{
+			ShutdownMode = ShutdownMode.OnExplicitShutdown;
+			DispatcherUnhandledException += ProcessUnhandledException;
+		}
 
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
 
-            if (Authenticate())
-            {
-                MainWindow mainWindow = new MainWindow(_messenger);
-                MainWindow = mainWindow;
-                mainWindow.Show();
-                mainWindow.Closed += MainWindow_Closed;
-            }
-            else
-            {
-                Shutdown();
-            }
-        }
+			if (Authenticate())
+			{
+				MainWindow mainWindow = new MainWindow(_messenger);
+				MainWindow = mainWindow;
+				mainWindow.Show();
+				mainWindow.Closed += MainWindow_Closed;
+			}
+			else
+			{
+				Shutdown();
+			}
+		}
 
-        public bool Authenticate()
-        {
-            string accessToken = Settings.Default.AccessToken;
+		public bool Authenticate()
+		{
+			string accessToken = Settings.Default.AccessToken;
 
-            if (!string.IsNullOrWhiteSpace(accessToken))
-            {
-                if (!_messenger.Authorize(accessToken))
-                {
-                    throw new Exception("Неправильный маркер доступа!");
-                }
-            }
-            else
-            {
-                AuthorizationWindow authWindow = new AuthorizationWindow();
-                authWindow.ShowDialog();
-                accessToken = authWindow.AccessToken;
+			if (!string.IsNullOrWhiteSpace(accessToken))
+			{
+				if (!_messenger.Authorize(accessToken))
+				{
+					throw new Exception("Неправильный маркер доступа!");
+				}
+			}
+			else
+			{
+				SetupWebBrowserEmulationVersion();
+				AuthorizationWindow authWindow = new AuthorizationWindow();
+				authWindow.ShowDialog();
+				accessToken = authWindow.AccessToken;
 
-                if (_messenger.Authorize(accessToken))
-                {
-                    Settings.Default.AccessToken = accessToken;
-                    Settings.Default.Save();
+				if (_messenger.Authorize(accessToken))
+				{
+					Settings.Default.AccessToken = accessToken;
+					Settings.Default.Save();
 
-                    MessageBox.Show("Авторизация прошла успешно!", "Авторизовано");
-                }
-                else
-                {
-                    return false;
-                }
-            }
+					MessageBox.Show("Авторизация прошла успешно!", "Авторизовано");
+				}
+				else
+				{
+					return false;
+				}
+			}
 
-            return true;
-        }
+			return true;
+		}
 
-        private void MainWindow_Closed(object sender, EventArgs e)
-        {
-            Shutdown();
-        }
+		private void SetupWebBrowserEmulationVersion()
+		{
+			string appName = Process.GetCurrentProcess().ProcessName + ".exe";
 
-        private void ProcessUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
-        {
-            string localAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData, Environment.SpecialFolderOption.Create);
+			RegistryKey regKey = null;
+			try
+			{
+				regKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION", true);
 
-            StringBuilder sb = new StringBuilder(localAppDataPath);
-            sb.Append(Path.DirectorySeparatorChar);
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            sb.Append(assembly.GetCustomAttribute<AssemblyCompanyAttribute>().Company);
-            sb.Append(Path.DirectorySeparatorChar);
-            sb.Append(assembly.GetCustomAttribute<AssemblyProductAttribute>().Product);
-            Directory.CreateDirectory(sb.ToString());
-            sb.Append(Path.DirectorySeparatorChar);
-            sb.Append(LOG_FILE_NAME);
-            string logFilePath = sb.ToString();
+				if (regKey == null)
+				{
+					return;
+				}
 
-            using (StreamWriter sw = File.CreateText(logFilePath))
-            {
-                sw.WriteLine(DateTime.Now.ToString() + " - Необработанное исключение:");
-                sw.WriteLine(e.Exception);
-            }
+				string findAppkey = Convert.ToString(regKey.GetValue(appName));
 
-            MessageBox.Show($"Приложение прекратило работу из-за непредвиденной ошибки. Посмотрите файл журнала \"{ logFilePath }\"", "Exception");
-        }
-    }
+				if (string.IsNullOrEmpty(findAppkey))
+				{
+					regKey.SetValue(appName, 11000, RegistryValueKind.DWord);
+				}
+				else if (findAppkey == "11000")
+				{
+					regKey.Close();
+					return;
+				}
+
+				findAppkey = Convert.ToString(regKey.GetValue(appName));
+
+				if (findAppkey != "11000")
+				{
+					throw new Exception($"Ошибка регистрации версии Internet Explorer: {findAppkey}");
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			finally
+			{
+				regKey?.Close();
+			}
+		}
+
+		private void MainWindow_Closed(object sender, EventArgs e)
+		{
+			Shutdown();
+		}
+
+		private void ProcessUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+		{
+			string localAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData, Environment.SpecialFolderOption.Create);
+
+			StringBuilder sb = new StringBuilder(localAppDataPath);
+			sb.Append(Path.DirectorySeparatorChar);
+			Assembly assembly = Assembly.GetExecutingAssembly();
+			sb.Append(assembly.GetCustomAttribute<AssemblyCompanyAttribute>().Company);
+			sb.Append(Path.DirectorySeparatorChar);
+			sb.Append(assembly.GetCustomAttribute<AssemblyProductAttribute>().Product);
+			Directory.CreateDirectory(sb.ToString());
+			sb.Append(Path.DirectorySeparatorChar);
+			sb.Append(LOG_FILE_NAME);
+			string logFilePath = sb.ToString();
+
+			using (StreamWriter sw = File.CreateText(logFilePath))
+			{
+				sw.WriteLine(DateTime.Now.ToString() + " - Необработанное исключение:");
+				sw.WriteLine(e.Exception);
+			}
+
+			MessageBox.Show($"Приложение прекратило работу из-за непредвиденной ошибки. Посмотрите файл журнала \"{ logFilePath }\"", "Exception");
+		}
+	}
 }
